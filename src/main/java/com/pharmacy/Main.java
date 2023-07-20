d909f02208839ad1ed4505f433bfa0d730695364
glymin <101063240+glymin@users.noreply.github.com>
2023-07-20 13:35:57 +0000

    Update Main.java

--- src/main/java/com/pharmacy/Main.java
+++ src/main/java/com/pharmacy/Main.java
@@ -4,7 +4,7 @@ package com.pharmacy;
  * Frederick Appiah- 10889066
  * Brefo Rexford - 10917663
  *OBED HOLALI ATTEPOR - 10880042
- *
+ *Glymin Terrence Worlali - 10901096
  */
 public class Main {
     public static void main(String[] args) {
